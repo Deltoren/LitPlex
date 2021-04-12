64e30c9d80ac15deca669c4c5334d8cf1f2d0b75
Albert Gibner <albert.gibner@csdevents.com>
2021-04-13 00:29:34 +0700

    Доделал макет главной страницы

--- Main_Activity.py
+++ Main_Activity.py
@@ -6,17 +6,27 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
+from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
-main_activity = Builder.load_file("program.kv")
-
-class MainScreen(BoxLayout):
-    pass
+authorLayout = Builder.load_file("authorLayout.kv")
+fileLayoutMacket = Builder.load_file("fileLayout.kv")
 
 class ProgramApp(App):
     def build(self):
-        return main_activity
+        mainLayout = BoxLayout()
+        fileLayout = fileLayoutMacket
+        for i in range(5):
+            btn = Button(text="Александр Сергеевич Пушкин")
+            btn.bind(on_press=self.on_press_button)
+            fileLayout.add_widget(btn)
+        mainLayout.add_widget(authorLayout)
+        mainLayout.add_widget(fileLayout)
+        return mainLayout
+
+    def on_press_button(self, instance):
+        print('Вы нажали на кнопку!')
 
 if __name__ == "__main__":
     ProgramApp().run()
