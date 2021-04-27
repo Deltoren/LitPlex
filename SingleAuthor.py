31d6351a6b3e0bfd7803e05f836ac987e7efc310
Albert Gibner <albert.gibner@csdevents.com>
2021-04-27 19:57:58 +0700

    Исправил ошибки: -в результате сляния веток возникли ошибки в коде, связанные с выводом информации об авторе

--- SingleAuthor.py
+++ SingleAuthor.py
@@ -15,19 +15,42 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
 authorLayout = Builder.load_file("singleAuthorPanel.kv")
-fileLayoutMacket = Builder.load_file("fileLayout.kv")
+infoAboutAuthorLayout = Builder.load_file("infoAboutAuthorLayout.kv")
 
-fileLayout = fileLayoutMacket
+infoAboutAuthorLayout = infoAboutAuthorLayout
 
 class ProgramApp(App):
     def build(self):
         mainLayout = BoxLayout()
         mainLayout.add_widget(authorLayout)
-        mainLayout.add_widget(fileLayout)
+        with open("./data.csv", encoding='utf-8') as r_file:
+            # Создаем объект DictReader, указываем символ-разделитель ","
+            file_reader = csv.DictReader(r_file)
+            # Считывание данных из CSV файла
+            count = 0
+            for row in file_reader:
+                if row['name'] == 'Марк Твен':
+                    if count > 5:
+                        break
+                    count += 1
+                    info = 'Имя: ' + row['name']
+                    if row['careers']:
+                        info += '\n' + 'Род деятельности: ' + ', '.join(row['careers'].split(','))
+                    if row['date_of_birthday']:
+                        info += '\n' + 'Дата рождения: ' + row['date_of_birthday']
+                    if row['languages']:
+                        info += '\n' + 'Языки произведений: ' + ', '.join(row['languages'].split(','))
+                    if row['genres']:
+                        info += '\n' + 'Жанры: ' + ', '.join(row['genres'].split(','))
+                    lbl = Label(text=info, font_size='20sp', size_hint=(.5, .5), pos_hint={'x':0.2, 'y':0.3})
+
+                    infoAboutAuthorLayout.add_widget(lbl)
+        mainLayout.add_widget(infoAboutAuthorLayout)
         return mainLayout
 
 
