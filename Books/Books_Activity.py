950660dfb8f30837aad51a6a7db53509e00c021c
Albert Gibner <albert.gibner@csdevents.com>
2021-05-05 19:19:07 +0700

    init commit - Создал директрию Books - Создал Books_Activity

---
+++ Books/Books_Activity.py
@@ -0,0 +1,31 @@
+import kivy
+import ScanningNames
+import csv
+import os
+import kivy
+import csv
+import os
+import Pasring.ParsingAuthor
+
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+
+def main():
+    BooksApp().run()
+
+class BooksApp(App):
+    def build(self):
+        mainBL = BoxLayout()
+        return mainBL
+
+if __name__ == "__main__":
+    BooksApp().run()
